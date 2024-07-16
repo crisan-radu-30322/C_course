88ce0684f1aeb47be24f0305312ed9b77efab723
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-07-16 14:34:04 +0300

    commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿int number1 = 0;
+int number2 = 0;
+
+//writes (...) in console
+Console.WriteLine("Give the first number");
+
+//declare string and assign value written in console
+string userinput = Console.ReadLine();
+//convert string to int
+number1 = int.Parse(userinput);
+
+Console.WriteLine("Give the second number");
+userinput = Console.ReadLine();
+number2 =  int.Parse(userinput);
+
+int result =  number1 + number2;
+
+//string concatenation (... + ... + ...)
+Console.WriteLine("The sum of " + number1 + " and " + number2 + " is " + result);
+
+//string interpolation
+Console.WriteLine($"The sum of {number1} and {number2} is {result}");
+
+//can use double instead of int tu use decimals 
+//int can convert to double 
+//double can't convert to int
