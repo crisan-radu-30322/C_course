b2e49c4d9f9d1de0edb08b5b0999760692e638c7
Radu <Crisan.So.Radu@student.utcluj.ro>
2024-12-23 14:15:34 +0200

    y

---
+++ EventsApp/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace EventsApp
+{
+
+    public delegate void TemperatureChangeHandler(string message);
+
+    public class TemperatureMonitor
+    {
+        public event TemperatureChangeHandler OnTemperatureChanged;
+
+        private int _temperature;
+        public int Temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                _temperature = value;
+                if(_temperature > 30)
+                {
+                    //RAISE EVENT!!!
+                    RaiseTemperatureChangedEvent("Temperature is above threshold");
+                }
+            }
+
+        }
+
+        protected virtual void RaiseTemperatureChangedEvent(string message)
+        {
+            OnTemperatureChanged?.Invoke(message);
+        }
+    }
+
+    public class TemperatureAlert
+    {
+        public void OnTemperatureChanged(string message)
+        {
+            Console.WriteLine("Alert " +  message);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureMonitor monitor = new TemperatureMonitor();
+            TemperatureAlert alert = new TemperatureAlert();
+            monitor.OnTemperatureChanged += alert.OnTemperatureChanged;
+
+            monitor.Temperature = 20;
+            Console.WriteLine("Please enter a temperature: ");
+            monitor.Temperature = int.Parse(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
