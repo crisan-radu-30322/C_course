1f8d8acd59fccb7f831371279ca7286bdf7acdc9
Radu <Crisan.So.Radu@student.utcluj.ro>
2025-03-17 17:43:00 +0200

    comit

---
+++ InvoiceManagement/LoginView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InvoiceManagement
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        public void Login_button_clicked(object sender, RoutedEventArgs e)
+        {
+            string passwordEntered = PasswordBox.Password;
+            string? envPw=Environment.GetEnvironmentVariable("InvoiceManagement");
+
+            if(envPw != null)
+            {
+                if(passwordEntered == envPw)
+                {
+                    MessageBox.Show("Entered Correct Password");
+                } else
+                {
+                    MessageBox.Show("Entered Wrong Password");
+                }
+            }
+            else
+            {
+                MessageBox.Show("DOESNT EXIST");
+            }
+        }
+    }
+}
