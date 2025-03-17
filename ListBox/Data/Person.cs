2ed62343918ea30c0b07751c6679bcf08a020530
Radu <Crisan.So.Radu@student.utcluj.ro>
2025-03-17 17:09:15 +0200

    commit

---
+++ ListBox/Data/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBox.Data
+{
+    public class Person
+    {
+        public int Age { set; get; }
+
+        public string Name { set; get; }
+    }
+}
---
+++ ListBox/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ListBox.Data;
+
+namespace ListBox;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public List<Person> People = new List<Person>
+    {
+        new Person{Name = "Radu", Age = 22},
+        new Person{Name = "Tudor", Age = 24}
+    };
+    public MainWindow()
+    {
+        InitializeComponent();
+        ListBoxPeople.ItemsSource = People;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var selectedItems = ListBoxPeople.SelectedItems;
+        foreach (var item in selectedItems)
+        {
+            var person = (Person)item;
+            MessageBox.Show(person.Name);
+        }
+    }
+}
